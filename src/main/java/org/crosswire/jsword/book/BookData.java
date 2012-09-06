5e28c88d73a03168ceeb33326214fde89887584c
chrisburrell <chris@burrell.me.uk>
2012-09-06 20:11:10 +0100

    Wrapping in non OSIS_ID elements

--- src/main/java/org/crosswire/jsword/book/BookData.java
+++ src/main/java/org/crosswire/jsword/book/BookData.java
@@ -21,6 +21,7 @@
  */
 package org.crosswire.jsword.book;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -31,6 +32,7 @@ import org.crosswire.common.util.Language;
 import org.crosswire.common.xml.JDOMSAXEventProvider;
 import org.crosswire.common.xml.SAXEventProvider;
 import org.crosswire.jsword.passage.Key;
+import org.jdom.Attribute;
 import org.jdom.Content;
 import org.jdom.Document;
 import org.jdom.Element;
@@ -219,16 +221,20 @@ public class BookData implements BookProvider {
                     Language lang = (Language) book.getProperty(BookMetaData.KEY_XML_LANG);
                     cell.setAttribute(OSISUtil.OSIS_ATTR_LANG, lang.getCode(), Namespace.XML_NAMESPACE);
                     row.addContent(cell);
-                    if (iters[i].hasNext()) {
-                        content = iters[i].next();
 
+                    StringBuilder newText = new StringBuilder(doDiffs ? 32 : 0);
+                    
+                    if (iters[i].hasNext()) {
+                        List<Content> contents = new ArrayList<Content>(1);
+
+                        do {
+                            content = iters[i].next();
+                            contents.add(content);
+                            addText(doDiffs, newText, content);
+                        } while(!hasOsisId(content));
+                        
                         if (doDiffs) {
-                            String thisText = "";
-                            if (content instanceof Element) {
-                                thisText = OSISUtil.getCanonicalText((Element) content);
-                            } else if (content instanceof Text) {
-                                thisText = ((Text) content).getText();
-                            }
+                            String thisText = newText.toString();
 
                             if (i > 0 && showDiffs[i - 1]) {
                                 List<Difference> diffs = new Diff(lastText, thisText, false).compare();
@@ -243,7 +249,7 @@ public class BookData implements BookProvider {
                             }
                             lastText = thisText;
                         }
-                        cell.addContent(content);
+                        cell.addContent(contents);
                         cellCount++;
                     }
                 }
@@ -261,6 +267,33 @@ public class BookData implements BookProvider {
         }
 
         return div;
+    }
+
+    private boolean hasOsisId(Content content) {
+        if(content instanceof Element) {
+            Attribute attribute = ((Element) content).getAttribute(OSISUtil.OSIS_ATTR_OSISID);
+            
+            if(attribute != null) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    private void addText(boolean doDiffs, StringBuilder newText, Content content) {
+        if(doDiffs) {
+            //if we already have content, let's add a space to avoid chaining words together
+            if(newText.length() != 0) {
+                newText.append(' ');
+            }
+            
+            if (content instanceof Element) {
+                newText.append(OSISUtil.getCanonicalText((Element) content));
+            } else if (content instanceof Text) {
+                newText.append(((Text) content).getText());
+            }
+        }
     }
 
     /**
