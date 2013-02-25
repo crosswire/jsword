1236dd667c3488b452dd5bb44d2260cae6524144
DM Smith <dmsmith@crosswire.org>
2013-02-25 14:54:30 -0500

    JS-198 Use basis for references in TSK Forgot to add VerseKey.

---
+++ src/main/java/org/crosswire/jsword/passage/VerseKey.java
@@ -0,0 +1,34 @@
+/**
+ * Distribution License:
+ * JSword is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License, version 2.1 as published by
+ * the Free Software Foundation. This program is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The License is available on the internet at:
+ *       http://www.gnu.org/copyleft/lgpl.html
+ * or by writing to:
+ *      Free Software Foundation, Inc.
+ *      59 Temple Place - Suite 330
+ *      Boston, MA 02111-1307, USA
+ *
+ * Copyright: 2013
+ *     The copyright to this program is held by it's authors.
+ *
+ */
+package org.crosswire.jsword.passage;
+
+import org.crosswire.jsword.versification.Versification;
+
+/**
+ * A Versified indicates that a Key has a Versification reference system.
+ *
+ * @see gnu.lgpl.License for license details.<br>
+ *      The copyright to this program is held by it's authors.
+ * @author DM Smith [dmsmith555 at yahoo dot com]
+ */
+public interface VerseKey extends Key {
+    Versification getVersification();
+}
