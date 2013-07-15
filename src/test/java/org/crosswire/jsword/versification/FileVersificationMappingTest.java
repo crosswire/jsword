d11b304246a81805b8c062bf6d40560522f64e84
chrisburrell <chris@burrell.me.uk>
2013-07-15 18:38:23 +0100

    Changes to mappings + Synodal, MT, Vulg

--- src/test/java/org/crosswire/jsword/versification/FileVersificationMappingTest.java
+++ src/test/java/org/crosswire/jsword/versification/FileVersificationMappingTest.java
@@ -1,21 +1,47 @@
 package org.crosswire.jsword.versification;
 
+import org.crosswire.jsword.versification.system.SystemSynodal;
+import org.crosswire.jsword.versification.system.SystemVulg;
+import org.junit.Assert;
 import org.crosswire.common.config.ConfigException;
 import org.crosswire.jsword.versification.system.SystemLeningrad;
 import org.crosswire.jsword.versification.system.Versifications;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * @author chrisburrell
  */
+@RunWith(Parameterized.class)
 public class FileVersificationMappingTest {
+    private String v11nName;
+
+    /**
+     * @param v11nName the v11n name we are testing
+     */
+    public FileVersificationMappingTest(String v11nName) {
+        this.v11nName = v11nName;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][]{
+                {SystemLeningrad.V11N_NAME},
+                {SystemSynodal.V11N_NAME},
+                {SystemVulg.V11N_NAME}, };
+        return Arrays.asList(data);
+    }
+
     @Test
-    public void testLeningrad() throws IOException, ConfigException {
-        final Versification versification = Versifications.instance().getVersification(SystemLeningrad.V11N_NAME);
+    public void testVersifications() throws IOException, ConfigException {
+        final Versification versification = Versifications.instance().getVersification(v11nName);
         FileVersificationMapping m = new FileVersificationMapping(versification);
-        new VersificationToKJVMapper(versification, m);
-        //TODO: fix test to track whether errors have occurred.
+        VersificationToKJVMapper mapper = new VersificationToKJVMapper(versification, m);
+        Assert.assertFalse(mapper.hasErrors());
     }
 }
