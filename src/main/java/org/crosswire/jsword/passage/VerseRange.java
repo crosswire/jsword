3b73ed3e848d243314d416511a635a0ef9c61a44
DM Smith <dmsmith@crosswire.org>
2015-02-07 16:12:51 -0500

    Minor javadoc cleanup and eclipse settings.

--- src/main/java/org/crosswire/jsword/passage/VerseRange.java
+++ src/main/java/org/crosswire/jsword/passage/VerseRange.java
@@ -29,8 +29,6 @@ import java.util.NoSuchElementException;
 import org.crosswire.common.icu.NumberShaper;
 import org.crosswire.jsword.versification.BibleBook;
 import org.crosswire.jsword.versification.Versification;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * A VerseRange is one step between a Verse and a Passage - it is a Verse plus a
@@ -51,7 +49,7 @@ public final class VerseRange implements VerseKey<VerseRange> {
      * provide this constructor however, you are supposed to provide a default
      * ctor for all beans. For this reason I suggest you don't use it.
      * 
-     *      * @param v11n
+     * @param v11n
      *            The versification for the range
      */
     public VerseRange(Versification v11n) {
@@ -959,12 +957,6 @@ public final class VerseRange implements VerseKey<VerseRange> {
      * The original string for picky users
      */
     private transient String originalName;
-
-    /**
-     * The log stream
-     */
-    /* pkg protected */static final transient Logger log = LoggerFactory.getLogger(VerseRange.class);
-
 
     /**
      * Serialization ID
