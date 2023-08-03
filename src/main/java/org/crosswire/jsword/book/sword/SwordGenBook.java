22708cf23213eadd4f09e31cc1267741e41545c9
Tuomas Airaksinen <tuomas.airaksinen@vrt.fi>
2023-08-03 20:05:51 +0300

    Make SwordGenBook constructor public

--- src/main/java/org/crosswire/jsword/book/sword/SwordGenBook.java
+++ src/main/java/org/crosswire/jsword/book/sword/SwordGenBook.java
@@ -52,7 +52,7 @@ public class SwordGenBook extends AbstractBook {
      * @param sbmd the metadata that describes the book
      * @param backend the means by which the resource is accessed
      */
-    SwordGenBook(SwordBookMetaData sbmd, Backend backend) {
+    public SwordGenBook(SwordBookMetaData sbmd, Backend backend) {
         super(sbmd, backend);
 
         if (backend == null) {
